b995ff360ff693ca4e848be6c6a33b3d396ae90c
Amirxon99 <114159472+Amirxon99@users.noreply.github.com>
2025-06-05 18:19:41 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Amirxon Ibaydillayev</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum quis, facilis delectus sint quam, earum animi eos inventore sunt iure, odio nostrum voluptas ut eveniet! Doloremque praesentium explicabo eaque amet quod qui dolores tempora deleniti nam repellat ipsum eos maxime nisi esse, ipsam vitae ducimus odio dicta alias fugiat suscipit.</p>
+      <ul>
+        <li>Html</li>
+        <li>Html</li>
+        <li>Html</li>
+        <li>Html</li>
+      </ul>
+      
+    </div>
+    
+  )
+}
+
+export default App
